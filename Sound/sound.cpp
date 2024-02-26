c06f7ca2e19e43adfdc6bfb13fec27a636622ee5
faiinrd <nopradakrit@gmail.com>
2024-02-26 23:24:14 +0700

    test sound

--- Sound/sound.cpp
+++ Sound/sound.cpp
@@ -1,9 +1,19 @@
 #include <windows.h>
 #include <mmsystem.h>
 #include <iostream>
+#include "../dialogueMaker/dialogueMaker.h"
 #pragma comment(lib, "winmm.lib")
 
 int main() {
-    PlaySound(TEXT("D:\\cpp\\project\\Tom-Khai\\Sound\\soundd\\magic.wav"), NULL, SND_FILENAME | SND_SYNC);
+
+    {PlaySound(TEXT("keyboard"), NULL, SND_FILENAME | SND_ASYNC); Sleep(5000);} //play 5 sec
+
+    // PlaySound(NULL, NULL, 0);
+    // Sleep(5000);
+
     return 0;
 }
+
+// cd "d:\cpp\project\Tom-Khai\Sound\"
+// g++ sound.cpp -o test -lwinmm
+//.\test
