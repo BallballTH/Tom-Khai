b699f8938ac23472b9d16e521c2d76de1da53646
Poon <zeepengineer@gmail.com>
2024-03-04 01:56:35 +0700

    print char lenght = width of terminal

--- dialogueMaker/dialogueMaker.h
+++ dialogueMaker/dialogueMaker.h
@@ -27,6 +27,8 @@ class Scene {
   Scene(std::string id, std::string dialogue, bool isEndScene=false);
   Scene(std::string id, std::string dialogue, bool isEndScene=false);
 
+  void Scene::printCharToTerminalWidth(char ch);
+  
   void printScene(); 
 
   void addOption(std::string text, std::string nextSceneId, std::string statchange,std::string event);
